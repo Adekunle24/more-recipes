cfee3089342e1b10060e9fe47d5af4827a766047
Olayinka Kunle <olayinkakunle24@gmail.com>
2017-08-22 18:15:59 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+//using express framework
+var express = require('express');
+
+//create express application
+var app = express();
+
+//set up handlebars view engine
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+
+//set server listening port 
+app.set('port',process.env.PORT || 3000);
+
+//directory for static files
+app.use(express.static(__dirname+'/public'));
+
+//load fortune library
+var fortune = require('./lib/fortune.js');
+
+//manage test
+app.use(function(req,res,next){
+res.locals.showTests = app.get('env') !== 'production' && req.query.text ==='1';
+next();
+
+});
+
+
+//add homepage route
+app.get('/',function(req,res)
+{
+    var data = { username: "Adekunle"};
+    res.render('home',data);
+});
+
+//add dashboard route
+app.get('/dashboard',function(req,res)
+{
+res.type('text/plain');
+res.send('My TeaserPlus application dashboard');
+});
+
+//custom 404 page
+app.use(function(req,res){
+res.type('text/plain');
+res.status(404);
+res.send('404 - Not found');
+});
+
+//custom 505 page
+app.use(function(err,req,res,next){
+console.log(err.stack);
+res.type('text/plain');
+res.status(500);
+res.send('500 - server error');
+});
+
+
+//start node JS Server
+app.listen(app.get('port'),function(){
+console.log('Express started on http://localhost: '+app.get('port')+ '; press Ctrl  - c to terminate.');
+});
