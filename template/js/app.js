5f0c399161f5963b82d62f23706e3b6fe7cfc68c
Olayinka Kunle <olayinkakunle24@gmail.com>
2017-08-25 23:09:31 +0100

    done with view-top-recipes

--- template/js/app.js
+++ template/js/app.js
@@ -5,6 +5,10 @@
  * framework NodeJS, Express and PostgreDb
  */
 
+//declare Angular variable scope for outside access 
+ var scope = angular.element($('.app-body')).scope();
+
+
 //Initiate JPinning 
 $('nav').jPinning();
 
@@ -67,9 +71,44 @@ $scope.LoginLoader = false;
 };
 //End login simulation
 
-});
+//simulate liking event
+$scope.LikeRecipe = function(element,loader){
+  var initial_value = element.html();
+  loader.removeClass('hide');
+  $timeout(function(){
+    element.html(parseInt(initial_value)+1);
+    loader.addClass('hide');
+  },1000);
+};
 
-//next element
+//simulate thumbs-down event
+$scope.DisLikeRecipe = function(element,loader){
+  var initial_value = element.html();
+  loader.removeClass('hide');
+  $timeout(function(){
+    element.html(parseInt(initial_value)+1);
+    loader.addClass('hide');
+  },1000);
+};
+
+
+//simulate love event
+$scope.LoveRecipe = function(element,loader){
+  var initial_value = element.html();
+  loader.removeClass('hide');
+  $timeout(function(){
+    element.html(parseInt(initial_value)+1);
+    loader.addClass('hide');
+  },1000);
+};
+
+
+
+});
+//End Angular controller
+
+
+//next element selector
 function nextInDOM(_subject, _selector) {
     var next = getNext(_subject);
     while(next.length != 0) {
@@ -95,7 +134,71 @@ function searchFor(_selector, _subject) {
     }
 }
 
+//previous element selector
+function prevInDOM(_subject, _selector) {
+    var previous = getPrev(_subject);
+    while(previous.length != 0) {
+        var found = searchFor(_selector, previous);
+        if(found != null) return found;
+        previous = getPrev(previous);
+    }
+    return null;
+}
+function getPrev(_subject) {
+    if(_subject.prev().length > 0) return _subject.prev();
+    return getPrev(_subject.parent());
+}
+
+
 
 $('.recipe-item-comment').click(function(){
   nextInDOM($(this), $('.comment-container')).toggleClass('hide');
 });
+//hide all recipe-items spinner
+$('.recipe-actions .fa-spin').addClass('hide');
+
+//simulate liking attempt
+$('.fa-thumbs-up').click(
+    function(){
+         var scope = angular.element($('.app-body')).scope();
+        var initial_value =  nextInDOM($(this), $('.action-value'));
+        var loader =  prevInDOM($(this), $('.fa-spin'));
+       scope.LikeRecipe(initial_value,loader);
+    }
+);
+
+//simulate dislike attempt
+$('.fa-thumbs-down').click(
+    function(){
+         var scope = angular.element($('.app-body')).scope();
+        var initial_value =  nextInDOM($(this), $('.action-value'));
+        var loader =  prevInDOM($(this), $('.fa-spin'));
+       scope.DisLikeRecipe(initial_value,loader);
+    }
+);
+//simulate love attempt
+$('.fa-heart').click(
+    function(){
+         var scope = angular.element($('.app-body')).scope();
+        var initial_value =  nextInDOM($(this), $('.action-value'));
+        var loader =  prevInDOM($(this), $('.fa-spin'));
+       scope.LoveRecipe(initial_value,loader);
+    }
+);
+//simulate random values for actions
+function PushRandomActionValues()
+{
+    var array = [];
+    var count = $('.action-value').length;
+    for(var b =0; b < count; b++)
+    {
+        var rnd = Math.ceil(Math.random()*100);
+        array.push(rnd);
+    }
+    var index = 0;
+    $('.action-value').each(function(){
+        $(this).html(array[index]);
+        index = index+1;
+    });
+}
+PushRandomActionValues();
