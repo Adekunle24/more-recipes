b6aa329b5bf47f1f37296b4958a410113c231b85
Olayinka Kunle <olayinkakunle24@gmail.com>
2017-08-25 21:29:35 +0100

    Almost done with view-top-recipes

--- template/js/app.js
+++ template/js/app.js
@@ -67,7 +67,35 @@ $scope.LoginLoader = false;
 };
 //End login simulation
 
+});
+
+//next element
+function nextInDOM(_subject, _selector) {
+    var next = getNext(_subject);
+    while(next.length != 0) {
+        var found = searchFor(_selector, next);
+        if(found != null) return found;
+        next = getNext(next);
+    }
+    return null;
+}
+function getNext(_subject) {
+    if(_subject.next().length > 0) return _subject.next();
+    return getNext(_subject.parent());
+}
+function searchFor(_selector, _subject) {
+    if(_subject.is(_selector)) return _subject;
+    else {
+        var found = null;
+        _subject.children().each(function() {
+            found = searchFor(_selector, $(this));
+            if(found != null) return false;
+        });
+        return found;
+    }
+}
 
 
-
+$('.recipe-item-comment').click(function(){
+  nextInDOM($(this), $('.comment-container')).toggleClass('hide');
 });
