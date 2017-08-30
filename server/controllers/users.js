dd13179a1a09cda48f4c2bcf1d860cc25110925e
Olayinka Kunle <olayinkakunle24@gmail.com>
2017-08-30 09:14:42 +0100

    Done with api users/signup

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,9 +6,14 @@ const getTotalUsers = (req,res) => {
 	);
 };
 const signUp = (req,res) => {
-	const username = req.params.username;
-	const email = req.params.email;
+	const usernameInput = req.params.username;
+	const emailInput = req.params.email;
 	const password = req.params.password;
+	userModel.create({
+		email : emailInput,
+		username : usernameInput,
+		passwordHash : password
+	}).then(output => res.send('Your account has been created successfully'));
 };
 const allMethods = { 'getTotalUsers' : getTotalUsers, 'signUp' : signUp};
 export default allMethods;
