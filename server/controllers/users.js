c1e89bdc75a175366007aa6c8821534a3a75a55f
Olayinka Kunle <olayinkakunle24@gmail.com>
2017-08-30 10:16:56 +0100

    Done with api users/signup and api users/signin

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,10 +1,17 @@
+// import all the models
 import allModels from '../models';
+
+// assign userModel to model user
 const userModel = allModels.user;
+
+// this is a test api to display all registered users
 const getTotalUsers = (req,res) => {
 	userModel.findAll().then(value => 
 		res.send(value)
 	);
 };
+
+// this method accepts username,email and password then creates a user account on the database
 const signUp = (req,res) => {
 	const usernameInput = req.params.username;
 	const emailInput = req.params.email;
@@ -15,5 +22,29 @@ const signUp = (req,res) => {
 		passwordHash : password
 	}).then(output => res.send('Your account has been created successfully'));
 };
-const allMethods = { 'getTotalUsers' : getTotalUsers, 'signUp' : signUp};
+
+// this method accepts username and password and then perform authentication 
+const signIn = (req,res) => {
+	const usernameInput = req.params.username;
+	const passwordInput = req.params.password;
+	userModel.findAll({
+		where: {
+			username : usernameInput
+		}
+	}).then(result => { 
+		if(result.length==0) {
+			res.send('Incorrect username or password');
+		}
+		else if (result.length === 1) {
+			if(passwordInput === result[0].passwordHash){
+				res.send('Login successful');
+			}
+			else{
+				res.send('Incorrect username or password');
+			}
+		
+		}   
+	});
+};
+const allMethods = { 'getTotalUsers' : getTotalUsers, 'signUp' : signUp, 'signIn' : signIn};
 export default allMethods;
--- server/routes/index.js
+++ server/routes/index.js
@@ -11,7 +11,7 @@ const usersController = controllers.usersController;
 
 // api routes messages
 const signUpMessage = 'Please provide username, email and password seperated by a forward slash e.g ..signup/andela/andela@yahoo.com/andelapassword to register';
-
+const signInMessage = 'use ..signin/your_username/your_password';
 
 // test api
 routes.get('/api/totalUsers', usersController.getTotalUsers);
@@ -19,6 +19,9 @@ routes.get('/api/totalUsers', usersController.getTotalUsers);
 // api-users-signup route
 routes.post('/api/users/signup/:username/:email/:password', usersController.signUp);
 routes.post('/api/users/signup', (req,res) => res.send(`This is the user-signup route.. ${signUpMessage}`));
-routes.get('/api/users/signup', (req,res) => res.send(`${signUpMessage}`));
+
+// api-users-signin route
+routes.post('/api/users/signin/:username/:password', usersController.signIn);
+routes.post('/api/users/signin', (req,res) => res.send(`This is the user-signin route.. ${signInMessage}`) );
 
 export default routes;
