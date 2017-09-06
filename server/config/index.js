b2803178ac56dc46b4ca7bd4ba2808035cac9a53
Olayinka Kunle <olayinkakunle24@gmail.com>
2017-09-06 18:55:08 +0100

    Fix Travis build errors

---
+++ server/config/index.js
@@ -0,0 +1,24 @@
+import env from 'dotenv';
+env.config();
+import fs from 'fs';
+import Sequelize from 'sequelize';
+import config from './config.json';
+import path from 'path';
+
+const envData = process.env.NODE_ENV || 'production';
+let sequelizeApp = new Sequelize({'dialect':'postgres'});
+
+if(envData==='production')
+{
+  sequelizeApp = new Sequelize(envData.DB_NAME,envData.DB_USERNAME,envData.DB_PASSWORD,{'dialect':envData.DIALECT,'host':envData.HOST });
+}
+else{
+  const configData = config[envData];
+  sequelizeApp = new Sequelize(configData.database,configData.username, configData.password,configData.options);
+}
+//const sequelizedUserModel =  sequelizeApp.import('../models/user.js');
+//const sequelizedRecipeModel = sequelizeApp.import('../models/recipe.js');
+const sequelizeObj = { 'sequelizeApp':sequelizeApp,'Sequelize':Sequelize
+};
+export default sequelizeObj;
+
