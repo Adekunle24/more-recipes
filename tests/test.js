0e6762cd3c65170eb45858c1cceb4b5455bf36fa
Olayinka Kunle <olayinkakunle24@gmail.com>
2017-09-07 18:37:42 +0100

    Fix travis build errors

--- tests/test.js
+++ tests/test.js
@@ -67,7 +67,7 @@ describe('API routes that manage users', () => {
 
   // test api/users/signin with true data
   it('api/users/signin should sign in successfully', (done) => {
-    server.post('/api/users/signin').send({ username: 'Master', password: 'alpha' }).expect('Content-type', /json/)
+    server.post('/api/users/signin').send({ username: 'TestUser', password: 'alpha' }).expect('Content-type', /json/)
       .expect(200)
       .end((err, res) => {
         console.log(JSON.stringify(res.body));
