0d512ad3249c7e558e7992b5e03558f6e4f387f3
Olayinka Kunle <olayinkakunle24@gmail.com>
2017-09-09 06:10:36 +0100

    heroku postbuild

--- server/models/index.js
+++ server/models/index.js
@@ -1,25 +1,35 @@
 
 import dotenv from 'dotenv';
-dotenv.config();
+
 import fs from 'fs';
 import Sequelize from 'sequelize';
 import path from 'path';
+
+dotenv.config();
+
 const basename = path.basename(module.filename);
 const db = {};
-let sequelizeApp = new Sequelize(`${process.env.DB_NAME}-${process.env.NODE_ENV.toLowerCase()}`,process.env.DB_USERNAME,
-   process.env.DB_PASSWORD,{dialect:process.env.DB_DIALECT,host:process.env.DB_HOST});
+let sequelizeApp;
+if (process.env.NODE_ENV !== 'production') {
+  sequelizeApp = new Sequelize(
+    `${process.env.DB_NAME}-${process.env.NODE_ENV.toLowerCase()}`, process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, { dialect: process.env.DB_DIALECT, host: process.env.DB_HOST }
+  );
+} else {
+  sequelizeApp = new Sequelize(process.env.DATABASE_URL);
+}
 fs
   .readdirSync(__dirname)
   .filter(file =>
     (file.indexOf('.') !== 0) &&
     (file !== basename) &&
     (file.slice(-3) === '.js'))
-  .forEach(file => {
+  .forEach((file) => {
     const model = sequelizeApp.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
